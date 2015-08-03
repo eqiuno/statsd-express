34865753346b03aa3e94fcd2f96e2d622f1a5354
liupeng <ian.liu@runmit.com>
2015-08-03 18:06:41 +0800

    add memory monitor

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ var onfinished = require('on-finished'),
 /**
  *  @param {string} host statsd host, default 127.0.0.1
  *  @param {integer} port statsd port, default 8125
+ *  @param {object} options
  *  @return {object} express middleware
  */
-exports        = module.exports = function (host, port) {
+exports        = module.exports = function (host, port, memory, reqKey, customized) {
     if (!host || typeof host === 'number') {
         host = '127.0.0.1';
         port = host;
@@ -24,14 +25,35 @@ exports        = module.exports = function (host, port) {
 
     var client = new lynx(host, port);
 
+    if (memory) {
+        var host = os.hostname();
+        recordMemory();
+    }
+
+    function recordMemory() {
+        var usage = process.memoryUsage();
+        client.count(host + '.vss', usage.rss);
+        client.count(host + '.heapUsed', usage.heapUsed);
+        client.count(host + '.heapUsage', usage.heapTotal);
+        setTimeout(recordMemory, 10000);
+    }
+
     return function (req, res, next) {
-        var _start  = new Date();
-        var _urlKey = _key(req);
-        onfinished(res, function (err, req) {
+        var _start    = new Date();
+        var keyMethod = _key;
+        if (reqKey && typeof reqKey === 'function') {
+            keyMethod = reqKey;
+        }
+        var _urlKey = keyMethod.call({}, req, res);
+        onfinished(res, function (err, res) {
             var respTime = new Date() - _start;
             client.timing(_urlKey, respTime);
             client.count(_urlKey, 1);
             client.count(res.status, 1);
+
+            if (customized && typeof customized === 'function') {
+                customized.call({}, res);
+            }
         });
         next();
     };
